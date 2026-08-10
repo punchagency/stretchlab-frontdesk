531a133b3b4cb3de59406672e68cb4bf4025f79f
Sirlahm <riliwanademola@yahoo.com>
2026-08-10 10:59:38 +0100

    update

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -17,7 +17,7 @@ const cookieDomain = import.meta.env.VITE_COOKIE_DOMAIN;
 export const setUserCookie = (token: string): void => {
   const expireAt = new Date();
   expireAt.setHours(expireAt.getHours() + 1);
-  Cookies.set("token", token, {
+  Cookies.set("frontdesk_token", token, {
     expires: expireAt,
     domain: cookieDomain,
     secure: true,
@@ -26,12 +26,12 @@ export const setUserCookie = (token: string): void => {
 };
 
 export const getUserCookie = (): string | null => {
-  return Cookies.get("token") || null;
+  return Cookies.get("frontdesk_token") || null;
 };
 
 export const deleteUserCookie = (): void => {
-  Cookies.remove("token", { domain: cookieDomain });
-  Cookies.remove("refresh_token", { domain: cookieDomain });
+  Cookies.remove("frontdesk_token", { domain: cookieDomain });
+  Cookies.remove("frontdesk_refresh_token", { domain: cookieDomain });
 };
 
 export const getUserInfo = (): CustomJwtPayload | null => {
@@ -45,7 +45,7 @@ export const getUserInfo = (): CustomJwtPayload | null => {
 export const setRefreshToken = (token: string): void => {
   const expireAt = new Date();
   expireAt.setDate(expireAt.getDate() + 7);
-  Cookies.set("refresh_token", token, {
+  Cookies.set("frontdesk_refresh_token", token, {
     expires: expireAt,
     domain: cookieDomain,
     secure: true,
@@ -54,5 +54,5 @@ export const setRefreshToken = (token: string): void => {
 };
 
 export const getRefreshToken = (): string | null => {
-  return Cookies.get("refresh_token") || null;
+  return Cookies.get("frontdesk_refresh_token") || null;
 };
